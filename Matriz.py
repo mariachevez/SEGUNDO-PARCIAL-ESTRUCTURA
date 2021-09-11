a697f67301fff1d095c15fe7f13b1d5d7aeb02ad
mariachevez <85723042+mariachevez@users.noreply.github.com>
2021-09-11 15:50:19 -0500

    Create Matriz.py

---
+++ Matriz.py
@@ -0,0 +1,18 @@
+class Matriz:
+    def __init__(self,matriz):
+        self.matriz=matriz
+    def ingresar(self,dato):
+        pass
+    def presentar (self):
+        for fila in range(len(self.matriz)):
+            for col in range(len(self.matriz[fila])):
+                # print(columna[col],end=" ")
+                print(self.matriz[fila][col],end=" ")
+            print()
+
+# columnas   0  1  2    0  1  2    0  1  2
+numeros=[[10,20,30],[60,80,90],[25,35,55]]
+# filas
+mat=Matriz(numeros)
+mat.presentar()
+# print(numeros[0],numeros[0][1])
